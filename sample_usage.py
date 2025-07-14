4ce06406fe0033d59427097cd3555fd87a7c9414
tk0987 <109526789+tk0987@users.noreply.github.com>
2025-07-15 01:50:58 +0200

    Update sample_usage.py

--- sample_usage.py
+++ sample_usage.py
@@ -6,7 +6,12 @@ except ImportError:
     
     
     """
-just in case... no comments, it is scratch as for now
+    architektura:
+    6 - po 1 noradrenaliny, 5 ach, 5 gaba input. out: 6 glutaminian, 6 gaba,6 ach, 2 opio, 1 nor.
+    12 - na out:  12 glutów, 12 gab, 4 opio i 4 nor.
+    
+    opcjonalna: 24 - jak wyżej.
+    i lustrzane odbicie, ale na końcu 6 glutów
     
     
     """
@@ -23,7 +28,24 @@ for _ in range(12):
 third_layout={}
 for _ in range(6):
     third_layout[f"var3_{_}"] = n.Neuron(norepinephrine_i=4,glutamate_i=12,glutamate_o=6,acetylcholine_i=6,gaba_i=6,opioid_i=4)
-    
+
+# input and output:
+inputs = []
+outputs = []
+# inps
+for i in range(6):
+    neuron = first_layout[f"var1_{i}"]
+    for nt in neuron.neurotransmitters:
+        input_array = getattr(neuron, f"{nt}_i", None)
+        if input_array is not None:
+            inputs.append(input_array)
+# outs
+for i in range(6):
+    neuron = third_layout[f"var1_{i}"]
+    for nt in neuron.neurotransmitters:
+        output_array = getattr(neuron, f"{nt}_i", None)
+        if output_array is not None:
+            outputs.append(output_array)
 
 for i in range(6):
     for j in range(12):
@@ -40,9 +62,9 @@ for i in range(12):
 
 for i in range(6):
     for j in range(12):
-        for nt in third_layout[f"var1_{i}"].neurotransmitters:
+        for nt in third_layout[f"var3_{i}"].neurotransmitters:
             n.Neuron.connect_neurons(
-                third_layout[f"var1_{i}"], second_layout[f"var2_{j}"], nt
+                third_layout[f"var3_{i}"], second_layout[f"var2_{j}"], nt
             )
 # this will be training...
 
